18729d0f320e5779298faa19ecdb86a74882f4f3
Ceiber Conrado Garibay Choque <ceiber@hotmail.com>
2021-10-07 12:39:47 -0500

    modificacion de conexion

--- src/java/Modelo/conexion.java
+++ src/java/Modelo/conexion.java
@@ -5,10 +5,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class conexion {
-    private final String baseDatos = "bdsys";
+    private final String baseDatos = "buboqcgafe5saicep2z0";
     private final String servidor = "jdbc:mysql://localhost/" + baseDatos;
-    private final String usuario = "root";
-    private final String clave = "";
+    private final String usuario = "u3tdcx9pzvmv7rkp";
+    private final String clave = "4r4AI0Zn7PMv8quJGnUU";
     
     public Connection conectar(){
         Connection cn = null;
